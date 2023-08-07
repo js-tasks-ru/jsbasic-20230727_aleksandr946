4045f414b66898d6008d88b8690433d6cdfa0df4
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-08-07 16:43:57 +0400

    Additional home task added

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,3 +4,18 @@ function isEmpty(obj) {
     return false;}
   return true;
 }
+
+// Дополнительное ДЗ 
+// из obj11 вывести в консоль значения свойств
+// используя только Object.keys и for
+/*
+let obj11 = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+for (let key in obj11) {console.log(obj11[key]);} 
+
+console.log(Object.values(obj11));
+*/
