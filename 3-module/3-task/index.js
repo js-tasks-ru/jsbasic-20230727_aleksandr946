338f3324885d8bb86dae4233568d0c49ce7f8869
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-08-09 23:56:17 +0400

    3m 3t solved

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
   // ваш код...
-}
+  let indexes = str.split('');
+  let res = '';
+  for (let i = 0; i < indexes.length; i++) {
+    if (indexes[i] === '-') {
+      res += indexes[i + 1].toUpperCase();
+      i++;}
+    else {res += indexes[i];}
+  }
+  return res;
+} 
