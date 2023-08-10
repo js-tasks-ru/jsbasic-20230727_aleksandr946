1de73ea108a7e649f6c8e837e285b9be7f3bb310
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-08-10 18:07:35 +0400

    3m 3t Added one more variant of the function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,6 +1,19 @@
 function camelize(str) {
   // ваш код...
-  let indexes = str.split('');
+  if (str.length > 0 && str.split('-').length > 0) {
+    let res = str.split('-').filter(e=>e !== '').map(e => e[0].toUpperCase() + e.slice(1)).join('');
+			
+    (str.startsWith('-')) ? (res = res[0].toUpperCase() + res.slice(1)) : (res = res[0].toLowerCase() + res.slice(1));
+    return res;
+  }
+  else {return str;}
+}
+
+/*
+Второй вариант решения
+
+function camelize(str) {
+	let indexes = str.split('');
   let res = '';
   for (let i = 0; i < indexes.length; i++) {
     if (indexes[i] === '-') {
@@ -9,4 +22,4 @@ function camelize(str) {
     else {res += indexes[i];}
   }
   return res;
-} 
+}  */
