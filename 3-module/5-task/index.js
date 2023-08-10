8d9657fac33f475a83c536f98ea98c1de91a7cc7
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-08-10 14:10:32 +0400

    definition min and max function realized

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
   // ваш код...
+  let numsArr = str.split(' ').filter(e => isFinite(e));
+  const maxMin = {};
+  maxMin.min = Math.min.apply(null, numsArr);
+  maxMin.max = Math.max.apply(null, numsArr);
+  return maxMin;
 }
