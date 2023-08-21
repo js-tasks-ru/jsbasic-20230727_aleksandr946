a04c9f1c844c4e0a4a0b30f21504740d2a5719f3
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-08-21 18:32:30 +0400

    initCarousel trouble

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,13 @@
 function initCarousel() {
   // ваш код...
-}
+  const rightArrow = document.querySelector('.carousel__arrow carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+
+  function rightArrowMove () {
+    carouselInner.style.transform = 'translateX(-300px)';
+  }
+  rightArrow.addEventListener('click', rightArrowMove);
+
+
+} 
