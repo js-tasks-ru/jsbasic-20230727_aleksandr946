c447e3ac607c4f3398402b9521c61a9f91d7ee95
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-08-22 15:41:51 +0400

    5m-3t carousel realized

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,13 +1,33 @@
 function initCarousel() {
-  // ваш код...
-  const rightArrow = document.querySelector('.carousel__arrow carousel__arrow_right');
-  let leftArrow = document.querySelector('.carousel__arrow carousel__arrow_left');
-  let carouselInner = document.querySelector('.carousel__inner');
+  const rightArrow = document.querySelector('.carousel__arrow_right');
+  const leftArrow = document.querySelector('.carousel__arrow_left');
+  leftArrow.style.display = 'none';
+  const carouselInner = document.querySelector('.carousel__inner');
+  let carouselInnerWidth = 0;
+  let carouselSlide = document.querySelector('.carousel__slide').offsetWidth;
+  let slidesAmount = carouselInner.children.length;
+  let numOfSlide = 1;
 
   function rightArrowMove () {
-    carouselInner.style.transform = 'translateX(-300px)';
+    if (numOfSlide < slidesAmount) {
+      leftArrow.style.display = '';
+      carouselInner.style.transform = 'translateX(' + (carouselInnerWidth - carouselSlide) + 'px)';
+      carouselInnerWidth = carouselInnerWidth - carouselSlide;
+      numOfSlide++;
+      if (numOfSlide == slidesAmount) {rightArrow.style.display = 'none';}
+    }
   }
+
+  function leftArrowMove () {
+    if (numOfSlide > 1) {
+      rightArrow.style.display = '';
+      carouselInner.style.transform = 'translateX(' + (carouselInnerWidth + carouselSlide) + 'px)';
+      carouselInnerWidth = carouselInnerWidth + carouselSlide;
+      numOfSlide--;
+      if (numOfSlide == 1) {leftArrow.style.display = 'none';}
+    }
+  }
+
   rightArrow.addEventListener('click', rightArrowMove);
-
-
-} 
+  leftArrow.addEventListener('click', leftArrowMove);
+}
